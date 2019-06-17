13efa919e0192efd03c859488e978996777dc709
Issa Soumare <isoumare@atixnet.fr>
2019-06-17 08:36:41 +0200

    add first component

---
+++ Components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {TextInput, View, Button } from 'react-native';
+
+class Search extends React.Component {
+    render(){
+        return (
+            <View>
+            <Button title="Rechercher" onPress={() => {}}/>
+            </View>
+        );
+    }
+}
+
+export default Search
